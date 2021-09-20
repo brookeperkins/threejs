b045a94c3437a2c1967b7184c02a59f63803145a
brookeperkins <brookemmperkins@gmail.com>
2021-09-19 21:35:44 -0700

    tutorial from @wrongakam

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className='logo'>
+        <span>REACT THREE FIBER</span>
+      </div>
+      <div className='episode'>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
